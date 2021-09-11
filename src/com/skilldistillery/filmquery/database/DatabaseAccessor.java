d1463313948e29b9cd83e18b820f9d8cd6bdd379
Dave Roberts <56813289+lohidenver@users.noreply.github.com>
2021-09-11 15:15:38 -0600

    Update DatabaseAccessor.java

--- src/com/skilldistillery/filmquery/database/DatabaseAccessor.java
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessor.java
@@ -1,20 +1,18 @@
-
 package com.skilldistillery.filmquery.database;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import com.skilldistillery.filmquery.entities.Actor;
 import com.skilldistillery.filmquery.entities.Film;
 
 public interface DatabaseAccessor {
-	public Film findFilmById(int filmId) throws SQLException;
+	public Film findFilmById(int filmId);
 
-	public Actor findActorById(int actorId) throws SQLException;
+	public Actor findActorById(int actorId);
 
-	public List<Actor> findActorsByFilmId(int filmId) throws SQLException;
+	public List<Actor> findActorsByFilmId(int filmId);
+
+	public List<Film> search(String sch);
 	
-	public List<Film> findFilmBySearch(String keyword) throws SQLException;
-	
-	public List<Film> searchFilm(String searchTerm) throws SQLException;
+	}
 }
