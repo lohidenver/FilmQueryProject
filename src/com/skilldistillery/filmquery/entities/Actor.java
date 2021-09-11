6a60ad237afe34e2ecef6f14350aa7f2949a266e
Dave Roberts <56813289+lohidenver@users.noreply.github.com>
2021-09-11 15:17:12 -0600

    Update Actor.java

--- src/com/skilldistillery/filmquery/entities/Actor.java
+++ src/com/skilldistillery/filmquery/entities/Actor.java
@@ -1,17 +1,16 @@
 package com.skilldistillery.filmquery.entities;
 
-import java.util.Objects;
 
 public class Actor {
-
 	private int id;
 	private String firstName;
 	private String lastName;
 	
-	public Actor(int id) {
-		this.id = id;
-		}
-
+	public Actor findActorById(int actorId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	public Actor(int id, String firstName, String lastName) {
 		super();
 		this.id = id;
@@ -22,58 +21,29 @@ public class Actor {
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append(firstName + " " + lastName);
-		
-		return sb.toString();
+		return "Actor: " + firstName + " " + lastName;
 	}
+	
+	
+	
 
-
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(firstName, id, lastName);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Actor other = (Actor) obj;
-		return Objects.equals(firstName, other.firstName) && id == other.id && Objects.equals(lastName, other.lastName);
-	}
-
-	public void setFilms(Object findFilmsByActorId) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
