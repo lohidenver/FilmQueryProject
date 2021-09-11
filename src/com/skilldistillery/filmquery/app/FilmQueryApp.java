4111124ec13e62f3fbbed8fa493fb8679c1d0a1c
Dave Roberts <56813289+lohidenver@users.noreply.github.com>
2021-09-11 15:14:48 -0600

    Update FilmQueryApp.java

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -1,112 +1,114 @@
 package com.skilldistillery.filmquery.app;
 
-	import java.sql.SQLException;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.InputMismatchException;
-	import java.util.List;
-	import java.util.Scanner;
+import java.util.List;
+import java.util.Scanner;
 
-	import com.skilldistillery.filmquery.database.DatabaseAccessor;
-	import com.skilldistillery.filmquery.database.DatabaseAccessorObject1;
-	import com.skilldistillery.filmquery.entities.Film;
-
+import com.skilldistillery.filmquery.database.DatabaseAccessor;
+import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
+import com.skilldistillery.filmquery.entities.Actor;
+import com.skilldistillery.filmquery.entities.Film;
 
 public class FilmQueryApp {
-  
-	DatabaseAccessor db = new DatabaseAccessorObject1();
+
+	DatabaseAccessor db = new DatabaseAccessorObject();
 
 	public static void main(String[] args) throws SQLException {
 		FilmQueryApp app = new FilmQueryApp();
-//		app.test();
 		app.launch();
 	}
 
-	private void test() throws SQLException {
-//		Film film = db.findFilmById(1);
-//		List<Actor> listOfActors = db.findActorsByFilmId(1);
-//		System.out.println(film);
-//		System.out.println(listOfActors);
-	}
-
-	private void launch() throws SQLException {
+	private void launch() {
 		Scanner input = new Scanner(System.in);
 
-		while (!startUserInterface(input))
-			;
+		startUserInterface(input);
 
 		input.close();
 	}
 
-	private boolean startUserInterface(Scanner input) throws SQLException {
-		DatabaseAccessorObject1 dao = new DatabaseAccessorObject1();
-		boolean exit = false;
-
-
-		System.out.println("              SD30db                     ");
-		System.out.println("                                         ");
-		System.out.println("  1. Find film by ID                     ");
-		System.out.println("  2. Find film by a search keyword.      ");
-		System.out.println("  3. Exit the application.               ");
-		System.out.println("                                         ");
-		System.out.println("\n\n");
-
-		System.out.print("Enter your choice:  ");
-		String choice = input.nextLine();
-
-		switch (choice) {
-		case "1":
-			System.out.print("\nEnter the film id: ");
-			int c1;
-			try {
-				c1 = input.nextInt();
-				Film film = db.findFilmById(c1);
-
-				if (film == null) {
-					System.out.println("The film was not found\n");
-
-				} else {
-					System.out.println(film);
-				}
-
-			} catch (InputMismatchException inputError) {
-				System.out.println("\nEnter a number for the film id.... Press Enter to go back to main menu.\n");
-				input.nextLine();
-
-			} finally {
-				input.nextLine();
-			}
-			break;
-		case "2":
-			System.out.print("Enter a keyword to find your film: ");
-
-			String c2 = input.nextLine();
-
-			List<Film> film = db.searchFilm(c2);
-
-			if (film.size() == 0) {
-				System.out.println("\nThe film was not found");
-
-			} else {
-				for (int i = 0; i < film.size(); i++) {
-					System.out.println(film.get(i).toString());
-				}
-
-			}
-			break;
-		case "3":
-			System.out.println("Enjoy your movie!");
-			exit = true;
-
-			break;
-
-		default:
-			System.out.println("Please enter a number: \n");
-
-			break;
-
+	private void startUserInterface(Scanner input) {
+		int user = 0;
+		System.out.println("\nMENU");
+		System.out.println("1. Look up film by ID");
+		System.out.println("2. Look up a film by search keyword");
+		System.out.println("3. Exit the application");
+		System.out.println();
+		try {
+			user = input.nextInt();
+		} catch (InputMismatchException e) {
+			input.nextLine();
+			System.out.println();
 		}
 
-		return exit;
+		switch (user) {
+		case 1:
+			filmById(input);
+
+			break;
+		case 2:
+			Scanner sc = new Scanner(System.in);
+			String search;
+			System.out.print("Enter search criteria: ");
+			search = sc.nextLine();
+			System.out.println();
+			List<Film> f = db.search(search);
+			if (f == null) {
+				System.err.println("Try a different search input\n");
+			} else {
+				for (Film f2 : f) {
+					System.out.println(f2);
+					if (f2.getActor() != null) {
+						for (Actor a : f2.getActor()) {
+							System.out.println(a);
+						}
+					}
+					else {
+						System.out.println("No actors");
+					}
+				}
+
+			}
+
+			break;
+		case 3:
+			System.out.println("exiting");
+			break;
+		default:
+			System.err.println("Not an option, try again");
+			break;
+		}
+		if (user != 3) {
+			launch();
+		}
+		
+
+	}
+	public void filmById(Scanner input) {
+		int id = 0;
+		System.out.print("Enter film id: ");
+		try {
+			id = input.nextInt();
+		} catch (InputMismatchException e) {
+			input.nextLine();
+			System.out.println();
+		}
+
+		Film film = db.findFilmById(id);
+		if (film == null) {
+			System.err.println("\nInvalid ID, please try again\n");
+			filmById(input);
+		} else {
+			System.out.println(film);
+			if(film.getActor() != null) {
+			for (Actor a  : film.getActor()) {
+				System.out.println(a);
+			}
+			}
+			else {
+				System.out.println("No actors");
+			}
+		}
 	}
 
 }
